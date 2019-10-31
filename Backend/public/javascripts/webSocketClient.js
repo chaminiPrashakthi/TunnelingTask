8c6ba9d7546849005e2c6e2565f9c6342e9fe5c5
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-31 15:16:32 +0530

    v2

--- Backend/public/javascripts/webSocketClient.js
+++ Backend/public/javascripts/webSocketClient.js
@@ -1,48 +1,56 @@
 var WebSocket = require('ws');
 const exec = require('child_process').exec;
-
+const io = require("socket.io-client"),
+const ioClient = io.connect("ws://15.206.88.74:8080");
 var message = null;
+
 //web socket connection
-const connection = new WebSocket('ws://15.206.88.74:8080');
-
-connection.onopen = () => {
-    console.log('connected');
+ioClient.on = () =>{
+    console.log('Connected');
 }
+// ioClient.on("seq-num", (msg) => console.info(msg));
 
-connection.onclose = () => {
-    console.error('disconnected');
-}
 
-connection.onmessage = e => {
-    console.log(e.data + 'Port No');
-    portForwarding(e.data);
-}
+// const connection = new WebSocket('ws://15.206.88.74:8080');
 
-connection.onerror = (error) => {
-    console.error('failed to connect', error);
-}
+// connection.onopen = () => {
+//     console.log('connected');
+// }
+
+// connection.onclose = () => {
+//     console.error('disconnected');
+// }
+
+// connection.onmessage = e => {
+//     console.log(e.data + 'Port No');
+//     portForwarding(e.data);
+// }
+
+// connection.onerror = (error) => {
+//     console.error('failed to connect', error);
+// }
 
 //ssh tunneling
 
-portForwarding = function (portVal) {
-    var
-        config = {
-            host: '15.206.88.74',
-            username: 'demo',
-            password: '1234'
-        },
-        portForwardingCmd = 'ssh -R ' + portVal + ':localhost:22 demo@' + config.host;
-    console.log(portForwardingCmd)
+// portForwarding = function (portVal) {
+//     var
+//         config = {
+//             host: '15.206.88.74',
+//             username: 'demo',
+//             password: '1234'
+//         },
+//         portForwardingCmd = 'ssh -R ' + portVal + ':localhost:22 demo@' + config.host;
+//     console.log(portForwardingCmd)
 
-    exec(portForwardingCmd, (error, stdout, stderr) => {
+//     exec(portForwardingCmd, (error, stdout, stderr) => {
 
-        if (error) {
-            message = 'Error';
-            console.log(message)
-            connection.send(message);
-            throw error;
-        }
-    });
-    connection.send(message);
+//         if (error) {
+//             message = 'Error';
+//             console.log(message)
+//             connection.send(message);
+//             throw error;
+//         }
+//     });
+//     connection.send(message);
 
-}
+// }
