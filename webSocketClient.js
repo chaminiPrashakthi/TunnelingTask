0dbd8dd1edd7ba906b0e3996a878f4be5253109e
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-11-01 10:09:53 +0530

    v1

---
+++ webSocketClient.js
@@ -0,0 +1,48 @@
+var WebSocket = require('ws');
+const exec = require('child_process').exec;
+
+var message = null;
+//web socket connection
+const connection = new WebSocket('ws://15.206.88.74:8080');
+
+connection.onopen = () => {
+    console.log('connected');
+}
+
+connection.onclose = () => {
+    console.error('disconnected');
+}
+
+connection.onmessage = e => {
+    console.log(e.data + 'Port No');
+    portForwarding(e.data);
+}
+
+connection.onerror = (error) => {
+    console.error('failed to connect', error);
+}
+
+//ssh tunneling
+
+portForwarding = function(portVal) {
+    var
+        config = {
+            host: '15.206.88.74',
+            username: 'demo',
+            password: '1234'
+        },
+        portForwardingCmd = 'ssh -R ' + portVal + ':localhost:22 demo@' + config.host;
+    console.log(portForwardingCmd)
+
+    exec(portForwardingCmd, (error, stdout, stderr) => {
+
+        if (error) {
+            message = 'Error';
+            console.log(message)
+            connection.send(message);
+            throw error;
+        }
+    });
+    connection.send(message);
+
+}
---
+++ webSocketServer.js
@@ -0,0 +1,51 @@
+const WebSocket = require('ws')
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use('/', router);
+app.listen(process.env.port || 8000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var portVal = null;
+var text;
+const wss = new WebSocket.Server({ port: 8080 })
+
+app.get('/', function (req, res) {
+    res.send('Success');
+});
+
+// connection establish
+// wss.on('connection', function (connection) {
+//     console.log('Opened connection ');
+
+
+
+//     app.post('/connection', function (req, res) {
+//         portVal = req.body.portVal;
+//         console.log('ssh with ' + portVal);
+//         // Send data back to the client
+//         connection.send(portVal);
+//         // data is received from client
+//         connection.on('message', function (message) {
+//             if (message != 'Error') {
+//                 console.log('Success')
+//                 text = 'Success'
+//             } else {
+//                 console.log('Error');
+//                 text = 'Error';
+//             }
+//             res.write("Connection " + text + " with port value " + portVal);
+//             res.end()
+//         });
+
+//     })
+
+//     // The connection was closed
+//     connection.on('close', function () {
+//         console.log('Closed Connection ');
+//     });
+
+// });
